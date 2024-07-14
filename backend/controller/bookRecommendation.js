1aa5fac234647bf5705a89047762253f4e859c0d
Punit Prajapati <punitprajapati2020@gmail.com>
2024-07-14 15:22:28 +0530

    Co-authored-by: Vanshil Joshi <vanshilJoshi@users.noreply.github.com>

--- backend/controller/bookRecommendation.js
+++ backend/controller/bookRecommendation.js
@@ -1,15 +1,15 @@
-const { analyzeUserPreferences } = require('./bookRecommendationAlgorithm')
-const UserLastBookModel = require('../models/userLastBook')
+const { analyzeUserPreferences } = require("./bookRecommendationAlgorithm");
+const UserLastBookModel = require("../models/userLastBook");
 
 const getRecommendedBooks = async (req, res) => {
-  const userId = req.userId
+  const userId = req.userId;
 
-  const userLastBook = await UserLastBookModel.findOne({ userId })
-  const { lastBorrowedBookId } = userLastBook
+  const userLastBook = await UserLastBookModel.findOne({ userId });
+  const { lastBorrowedBookId } = userLastBook;
 
-  const result = await analyzeUserPreferences(userId, lastBorrowedBookId)
+  const result = await analyzeUserPreferences(userId, lastBorrowedBookId);
 
-  res.status(200).json({ totalHits: result.length, data: result })
-}
+  res.status(200).json({ totalHits: result.length, data: result });
+};
 
-module.exports = { getRecommendedBooks }
+module.exports = { getRecommendedBooks };
